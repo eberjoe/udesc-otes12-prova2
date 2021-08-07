209e12d89bfaf9430332ee021bc456ac50ab1946
eberjoe <eberjoe@gmail.com>
2021-08-07 16:15:47 -0300

    Add get endpoints

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -41,12 +41,63 @@ endpoints.put('/update-venda/:id', async (req: Request, res: Response) => {
 endpoints.delete('/delete-venda/:id', async (req:Request, res: Response) => {
   try {
     const id = Number(req.params.id);
-    const deletedVenda = await prisma.vendas.delete({
+    await prisma.vendas.delete({
       where: {
         id: id
       }
     });
     res.status(204).send();
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+endpoints.get('/monitor-func/:func', async (req: Request, res: Response) => {
+  try {
+    const func = req.params.func;
+    const vendasByFunc = await prisma.vendas.findMany({
+      where: {
+        funcionario: func
+      }
+    });
+    res.status(200).json(vendasByFunc);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+endpoints.get('/monitor-reg/:reg', async (req: Request, res: Response) => {
+  try {
+    const reg = req.params.reg;
+    const vendasByReg = await prisma.vendas.findMany({
+      where: {
+        regiao: reg
+      }
+    });
+    res.status(200).json(vendasByReg);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+endpoints.get('/monitor-tri/:tri', async (req: Request, res: Response) => {
+  try {
+    const tri = req.params.tri;
+    const vendasByTri = await prisma.vendas.findMany({
+      where: {
+        trimestre: tri
+      }
+    });
+    res.status(200).json(vendasByTri);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+endpoints.get('/all', async (req: Request, res: Response) => {
+  try {
+    const allVendas = await prisma.vendas.findMany();
+    res.status(200).json(allVendas);
   } catch (e) {
     res.status(500).send(e.message);
   }
